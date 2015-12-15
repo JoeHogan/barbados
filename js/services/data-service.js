4453ee832efb666511479e6ab41a5ef367306536
Joe Hogan <joseph.t.hogan@gmail.com>
2015-12-15 13:45:56 -0500

    Revert "Merge remote-tracking branch 'cretney/develop' into develop"
    This reverts commit c021768f2d586728bb17907470955cc4105bad7a, reversing
    changes made to 1e9a039ab41498befa2889f2a2029230c9b4c57e.

--- js/services/data-service.js
+++ js/services/data-service.js
@@ -2,44 +2,21 @@
 
 	var dataService = angular.module('dataService', [])
 
-	.factory('dataService-Prod', ['$filter','$http','$q', function($filter,$http,$q) {
-		return {
-			getForm: function(id){
-				return $http.get("http://52.25.174.100/api/Forms/GetForm/"+id);
-			},
-			getForms: function(){
-				return $http.get("http://52.25.174.100/api/Forms/GetBlockIDs");
-			},
-			postForm: function(data,id){
-				if(id && !data.blockId) data.blockId = id;
-				return $http.post("http://52.25.174.100/api/Forms/PostForm",{formData: JSON.stringify(data)});
-			},
-			getLists: function(id,types){
-				var t = types || ['country','unitsOfMeasure','suppliers','reason','comment','recommendation','condition','companies','formsMap','approvalCodes'];
-				return $http.get("http://52.25.174.100/api/Forms/GetLists?blockId="+id+"&listTypes="+t.join(','), {cache: true});
-			}
-		}
-	}])
-
 	.factory('dataService', ['$filter','$http','$q', function($filter,$http,$q) {
 		return {
 			getForm: function(id){
-				var def = $q.defer();
-				this.getForms().then(function(data){
-					var item = $filter('filter')(data.data, {blockId: parseInt(id)}, true)[0];
-					def.resolve({data: item});
-				});
-				return def.promise;
+				return $http.get("http://52.25.174.100/Forms/GetForm/"+id);
 			},
 			getForms: function(){
-				return $http.get("sample-data/forms.json");
+				return $http.get("http://52.25.174.100/Forms/GetFormData");
 			},
 			postForm: function(data,id){
 				if(id && !data.blockId) data.blockId = id;
-				return $http.post("http://52.25.174.100/api/Forms/PostForm",{formData: JSON.stringify(data)});
+				return $http.post("http://52.25.174.100/Forms/PostForm",{formData: JSON.stringify(data)});
 			},
 			getLists: function(id,types){
-				return $http.get("sample-data/lists.json");
+				var t = types || ['country','unitsOfMeasure','suppliers','reason','comment','recommendation','condition','organisations','formsMap','approvalCodes'];
+				return $http.get("http://52.25.174.100/Forms/GetLists?blockId="+id+"&listTypes="+t.join(','), {cache: true});
 			}
 		}
 	}]);
